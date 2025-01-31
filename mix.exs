83b11eeaab0b2e50adf0417201add50a5955f467
Paulo Curado <phcurado@gmail.com>
2025-01-31 11:06:26 +0200

    release 0.3.0 🚀

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule DaisyUIComponents.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/phcurado/daisy_ui_components"
-  @version "0.2.1"
+  @version "0.3.0"
 
   def project do
     [
