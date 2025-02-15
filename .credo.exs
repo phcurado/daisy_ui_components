a2b1e088f9b36f87b829ecdbdf82e6144057cf02
Paulo Curado <phcurado@gmail.com>
2025-02-15 09:02:27 +0200

    improve credo file

--- .credo.exs
+++ .credo.exs
@@ -102,7 +102,7 @@
           {Credo.Check.Readability.ModuleDoc, []},
           {Credo.Check.Readability.ModuleNames, []},
           {Credo.Check.Readability.ParenthesesInCondition, []},
-          {Credo.Check.Readability.ParenthesesOnZeroArityDefs, []},
+          {Credo.Check.Readability.ParenthesesOnZeroArityDefs, false},
           {Credo.Check.Readability.PipeIntoAnonymousFunctions, []},
           {Credo.Check.Readability.PredicateFunctionNames, []},
           {Credo.Check.Readability.PreferImplicitTry, []},
