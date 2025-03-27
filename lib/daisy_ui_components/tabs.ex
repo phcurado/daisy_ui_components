8421e2da7e5b256ac7f9c73b10e50b494dc548be
Michael Kompanets <mkompanets@gmail.com>
2025-03-26 23:40:17 -0700

    changing approach in favor of tab component

--- lib/daisy_ui_components/tabs.ex
+++ lib/daisy_ui_components/tabs.ex
@@ -1,152 +1,226 @@
 defmodule DaisyUIComponents.Tabs do
   @moduledoc """
-  Tab component
+  Tab component used to show a list of links in a tabbed format.
 
   Renders a list of tab links or toggles with radio toggle and content or label with radio toggle and content
 
   https://daisyui.com/components/tab/
+
+  ## Basic Example:
+      <.tabs>
+        <.tab title="Tab 1" />
+        <.tab title="Tab 2" active />
+        <.tab title="Tab 3" disabled />
+      </.tabs>
+
+  ## Renders:
+      <div role="tablist" class="tabs">
+        <a role="tab" class="tab">Tab 1</a>
+        <a role="tab" class="tab tab-active">Tab 2</a>
+        <a role="tab" class="tab tab-disabled">Tab 3</a>
+       </div>
+
+  ## Basic Example with inner block content
+  ## NOTE: if title attribute is provided, the inner block will not be rendered
+      <.tabs>
+        <.tab>
+          <span>extra content</span>
+          Tab 1
+        </.tab>
+        <.tab active>
+          Tab 2
+        </.tab>
+        <.tab disabled>
+          Tab 3
+        </.tab>
+      </.tabs>
+
+  ## Renders:
+
+      <div role="tablist" class="tabs">
+        <a role="tab" class="tab">
+          <span>extra content</span>
+          Tab 1
+        </a>
+        <a role="tab" class="tab tab-active">
+          Tab 2
+        </a>
+        <a role="tab" class="tab tab-disabled">
+          Tab 3
+        </a>
+       </div>
+
+  ## Tabs with button type
+      <.tabs>
+        <.tab type="button" title="Tab 1" />
+        <.tab type="button" title="Tab 2" active />
+        <.tab type="button" title="Tab 3" disabled />
+      </.tabs>
+
+  ## Renders:
+
+      <div role="tablist" class="tabs">
+        <button class="tab">Tab 1</button>
+        <button class="tab tab-active">Tab 2</button>
+        <button class="tab tab-disabled">Tab 3</button>
+      </div>
+
+  ## Tabs with radio type
+  ## These tabs must provide a title attribute
+      <.tabs>
+        <.tab type="radio" title="Tab 1" name="tabs" />
+        <.tab type="radio" title="Tab 2" name="tabs" active />
+        <.tab type="radio" title="Tab 3" name="tabs" disabled />
+      </.tabs>
+
+  ## Renders:
+      <div role="tablist" class="tabs">
+        <input type="radio" name="tabs" aria-label="Tab 1" />
+        <input type="radio" name="tabs" aria-label="Tab 2" checked="checked" />
+        <input type="radio" name="tabs" aria-label="Tab 3" disabled />
+      </div>
+
   """
 
   use DaisyUIComponents, :component
 
-  @doc """
-  Renders a Tabs with links as toggle by default.
-
-  ## Default Example
-  <.tabs>
-    <:tab>Tab 1</:tab>
-    <:tab active>Tab 2</:tab>
-    <:tab disabled>Tab 3</:tab>
-  </.tabs>
-
-  ## Renders
-  <div role="tablist" class="tabs">
-    <a role="tab" class="tab">Tab 1</a>
-    <a role="tab" class="tab tab-active">Tab 2</a>
-    <a role="tab" class="tab tab-disabled">Tab 3</a>
-  </div>
-
-  ## Radio Example
-  ## https://daisyui.com/components/tab/#radio-tabs-border--tab-content
-  <.tabs type="radio">
-    <:tab>Tab 1</:tab>
-    <:tab active>Tab 2</:tab>
-    <:tab>Tab 3</:tab>
-  </.tabs>
-
-  ## Renders
-  <div class="tabs">
-    <input type="radio" name="tabs" class="tab" aria-label="Tab 1">
-    <input type="radio" name="tabs" class="tab" aria-label="Tab 2" checked="checked">
-    <input type="radio" name="tabs" class="tab" aria-label="Tab 3">
-  </div>
-
-  ## Label Example
-  ## https://daisyui.com/components/tab/#radio-tabs-lift-with-icons--tab-content
-  <.tabs type="label">
-    <:tab>Tab 1</:tab>
-    <:tab active>Tab 2</:tab>
-    <:tab>Tab 3</:tab>
-  </.tabs>
-
-  ## Renders
-  <div class="tabs">
-    <label class="tab">
-      <input type="radio" name="tabs" class="tab"> Tab 1
-    </label>
-    <label class="tab">
-      <input type="radio" name="tabs" class="tab" checked="checked"> Tab 2
-    </label>
-    <label class="tab">
-      <input type="radio" name="tabs" class="tab"> Tab 3
-    </label>
-  </div>
-  """
-
-  attr :type, :string, values: ~w(link radio label), default: "link"
-  attr :name, :string, default: nil
   attr :class, :any, default: nil
   attr :size, :string, values: sizes()
   attr :tabs_style, :string, values: ~w(box border lift)
   attr :tabs_position, :string, values: ~w(top bottom)
   attr :rest, :global
 
-  slot :tab do
-    attr :active, :boolean
-    attr :disabled, :boolean
-  end
-
-  slot :tab_content
-
   slot :inner_block
 
-  def tabs(%{type: "radio"} = assigns) do
-    assigns =
-      assign(assigns, :class, tabs_classes(assigns))
-
-    ~H"""
-    <div class={@class} {@rest}>
-      <%= for {tab, index} <- Enum.with_index(@tab) do %>
-        <.tab_radio
-          name={@name}
-          class={radio_tab_classes(tab)}
-          aria_label={render_slot(tab)}
-          active={tab[:active]}
-        />
-
-        <% tab_content = Enum.at(@tab_content, index) %>
-        <.tab_content :if={tab_content} class={tab_content_classes(tab_content)}>
-          {render_slot(tab_content)}
-        </.tab_content>
-      <% end %>
-
-      {render_slot(@inner_block)}
-    </div>
-    """
-  end
-
-  def tabs(%{type: "label"} = assigns) do
-    assigns =
-      assign(assigns, :class, tabs_classes(assigns))
-
-    ~H"""
-    <div class={@class} {@rest}>
-      <%= for {tab, index} <- Enum.with_index(@tab) do %>
-        <label class={label_tab_classes(tab)}>
-          <.tab_radio name={@name} class={radio_tab_classes(tab)} active={tab[:active]} />
-          {render_slot(tab)}
-        </label>
-
-        <% tab_content = Enum.at(@tab_content, index) %>
-        <.tab_content :if={tab_content} class={tab_content_classes(tab_content)}>
-          {render_slot(tab_content)}
-        </.tab_content>
-      <% end %>
-
-      {render_slot(@inner_block)}
-    </div>
-    """
-  end
-
-  # This is default for tabs with links. From the examples, tab content is not rendered as part of this block
   def tabs(assigns) do
     assigns =
       assign(assigns, :class, tabs_classes(assigns))
 
     ~H"""
     <div role="tablist" class={@class} {@rest}>
-      <.tab_link :for={tab <- @tab} class={tab_classes(tab)}>
-        {render_slot(tab)}
-      </.tab_link>
       {render_slot(@inner_block)}
     </div>
     """
   end
 
+  attr :title, :string
+  attr :class, :any, default: nil
+  attr :type, :string, values: ~w(link button radio label), default: "link"
+  attr :name, :string, default: nil
+  attr :active, :boolean, default: false
+  attr :disabled, :boolean, default: false
+  attr :rest, :global
+
+  slot :inner_block
+
+  def tab(%{type: "radio", title: _title} = assigns) do
+    assigns =
+      assign(assigns, :class, radio_tab_classes(assigns))
+
+    ~H"""
+    <.tab_radio title={@title} name={@name} class={@class} active={@active} {@rest} />
+    """
+  end
+
+  def tab(%{type: "radio"} = _assigns) do
+    raise ArgumentError,
+          "title attribute is required for radio type tab. Radio type tab cannot have inner block content"
+  end
+
+  def tab(%{type: "button", title: _title} = assigns) do
+    assigns =
+      assign(assigns, :class, tab_classes(assigns))
+
+    ~H"""
+    <.tab_button title={@title} class={@class} {@rest} />
+    """
+  end
+
+  def tab(%{type: "button"} = assigns) do
+    assigns =
+      assign(assigns, :class, tab_classes(assigns))
+
+    ~H"""
+    <.tab_button class={@class} {@rest}>
+      {render_slot(@inner_block)}
+    </.tab_button>
+    """
+  end
+
+  def tab(%{title: _title} = assigns) do
+    assigns =
+      assign(assigns, :class, tab_classes(assigns))
+
+    ~H"""
+    <.tab_link title={@title} class={@class} {@rest} />
+    """
+  end
+
+  def tab(assigns) do
+    assigns =
+      assign(assigns, :class, tab_classes(assigns))
+
+    ~H"""
+    <.tab_link class={@class} {@rest}>
+      {render_slot(@inner_block)}
+    </.tab_link>
+    """
+  end
+
+  attr :title, :string, required: true
+  attr :name, :string, required: true
+  attr :class, :any, default: nil
+  attr :active, :boolean, default: false
+  attr :rest, :global
+
+  def tab_radio(assigns) do
+    ~H"""
+    <input
+      type="radio"
+      name={@name}
+      class={@class}
+      aria-label={@title}
+      checked={@active && "checked"}
+      {@rest}
+    />
+    """
+  end
+
+  attr :title, :string
   attr :class, :any, default: nil
   attr :rest, :global
 
   slot :inner_block
+
+  def tab_button(%{title: _title} = assigns) do
+    ~H"""
+    <button class={@class} {@rest}>
+      {@title}
+    </button>
+    """
+  end
+
+  def tab_button(assigns) do
+    ~H"""
+    <button class={@class} {@rest}>
+      {render_slot(@inner_block)}
+    </button>
+    """
+  end
+
+  attr :title, :string
+  attr :class, :any, default: nil
+  attr :rest, :global
+
+  slot :inner_block
+
+  def tab_link(%{title: _title} = assigns) do
+    ~H"""
+    <a role="tab" class={@class} {@rest}>
+      {@title}
+    </a>
+    """
+  end
 
   def tab_link(assigns) do
     ~H"""
@@ -157,30 +231,14 @@ defmodule DaisyUIComponents.Tabs do
   end
 
   attr :class, :any, default: nil
-  attr :name, :string, default: nil
-  attr :active, :boolean, default: false
-  attr :aria_label, :string, default: nil
-  attr :rest, :global
-
-  def tab_radio(assigns) do
-    ~H"""
-    <input
-      type="radio"
-      name={@name}
-      class={@class}
-      aria-label={@aria_label}
-      checked={@active && "checked"}
-      {@rest}
-    />
-    """
-  end
-
-  attr :class, :any, default: nil
   attr :rest, :global
 
   slot :inner_block
 
   def tab_content(assigns) do
+    assigns =
+      assign(assigns, :class, tab_content_classes(assigns))
+
     ~H"""
     <div class={@class} {@rest}>
       {render_slot(@inner_block)}
@@ -200,32 +258,26 @@ defmodule DaisyUIComponents.Tabs do
   end
 
   # Individual Tab Classes
-  defp tab_classes(tab) do
+  defp tab_classes(assigns) do
     classes([
       "tab",
-      maybe_add_class(tab[:active], "tab-active"),
-      maybe_add_class(tab[:disabled], "tab-disabled")
+      maybe_add_class(assigns[:active], "tab-active"),
+      maybe_add_class(assigns[:disabled], "tab-disabled")
     ])
   end
 
-  defp tab_content_classes(tab_content) do
+  defp radio_tab_classes(assigns) do
+    # Instead of "tab-active" class, the checked attribute is used for radio tabs
+    classes([
+      "tab",
+      maybe_add_class(assigns[:disabled], "tab-disabled")
+    ])
+  end
+
+  defp tab_content_classes(assigns) do
     classes([
       "tab-content",
-      tab_content[:class]
-    ])
-  end
-
-  defp radio_tab_classes(tab) do
-    classes([
-      "tab",
-      tab[:class]
-    ])
-  end
-
-  defp label_tab_classes(tab) do
-    classes([
-      "tab",
-      tab[:class]
+      assigns.class
     ])
   end
 
--- test/daisy_ui_components/tabs_test.exs
+++ test/daisy_ui_components/tabs_test.exs
@@ -7,164 +7,283 @@ defmodule DaisyUIComponents.TabsTest do
 
   alias DaisyUIComponents.Utils
 
-  test "tabs" do
-    assigns = %{}
+  describe "tabs with links (default)" do
+    test "basic default tabs with title" do
+      assigns = %{}
 
-    ~H"""
-    <.tabs>
-      <:tab>Tab 1</:tab>
-      <:tab active>Tab 2</:tab>
-      <:tab disabled>Tab 3</:tab>
-    </.tabs>
-    """
-    |> parse_component()
-    |> assert_component("div")
-    |> assert_class("tabs")
-    |> select_element("a", fn [first, second, third] ->
-      first_text =
-        [first]
-        |> assert_component("a")
-        |> assert_class("tab")
-        |> text()
+      component =
+        ~H"""
+        <.tabs>
+          <.tab title="Tab 1" />
+          <.tab title="Tab 2" active />
+          <.tab title="Tab 3" disabled />
+        </.tabs>
+        """
 
-      assert first_text =~ "Tab 1"
+      expected_component =
+        ~H"""
+        <div role="tablist" class="tabs">
+          <a role="tab" class="tab">Tab 1</a>
+          <a role="tab" class="tab tab-active">Tab 2</a>
+          <a role="tab" class="tab tab-disabled">Tab 3</a>
+        </div>
+        """
 
-      second_text =
-        [second]
-        |> assert_component("a")
-        |> assert_class("tab tab-active")
-        |> text()
+      assert_components(component, expected_component)
+    end
 
-      assert second_text =~ "Tab 2"
+    test "basic default tabs with inner block" do
+      assigns = %{}
 
-      third_text =
-        [third]
-        |> assert_component("a")
-        |> assert_class("tab tab-disabled")
-        |> text()
+      component =
+        ~H"""
+        <.tabs>
+          <.tab>
+            Tab 1 <span>Extra content</span>
+          </.tab>
+          <.tab active>Tab 2</.tab>
+          <.tab disabled>Tab 3</.tab>
+        </.tabs>
+        """
 
-      assert third_text =~ "Tab 3"
-    end)
-  end
+      expected_component =
+        ~H"""
+        <div role="tablist" class="tabs">
+          <a role="tab" class="tab">Tab 1 <span>Extra content</span></a>
+          <a role="tab" class="tab tab-active">Tab 2</a>
+          <a role="tab" class="tab tab-disabled">Tab 3</a>
+        </div>
+        """
 
-  test "tabs sizes" do
-    for size <- Utils.sizes() do
-      assigns = %{size: size}
+      assert_components(component, expected_component)
+    end
 
-      ~H"""
-      <.tabs size={@size}>
-        <:tab>Tab 1</:tab>
-      </.tabs>
-      """
-      |> parse_component()
-      |> assert_component("div")
-      |> assert_class("tabs tabs-#{size}")
+    test "basic default tabs ignores inner block when title is provided" do
+      assigns = %{}
+
+      component =
+        ~H"""
+        <.tabs>
+          <.tab title="Tab 1">
+            Ignored Tab 1 <span>Extra content</span>
+          </.tab>
+          <.tab title="Tab 2" active>
+            Ignored Tab 2
+          </.tab>
+          <.tab title="Tab 3" disabled>
+            Ignored Tab 3
+          </.tab>
+        </.tabs>
+        """
+
+      expected_component =
+        ~H"""
+        <div role="tablist" class="tabs">
+          <a role="tab" class="tab">Tab 1</a>
+          <a role="tab" class="tab tab-active">Tab 2</a>
+          <a role="tab" class="tab tab-disabled">Tab 3</a>
+        </div>
+        """
+
+      assert_components(component, expected_component)
+    end
+
+    test "tabs sizes" do
+      for size <- Utils.sizes() do
+        assigns = %{size: size}
+
+        ~H"""
+        <.tabs size={@size}>
+          <.tab>Tab 1</.tab>
+        </.tabs>
+        """
+        |> parse_component()
+        |> assert_component("div")
+        |> assert_class("tabs tabs-#{size}")
+      end
+    end
+
+    test "tabs styles" do
+      for style <- ~w(box border lift) do
+        assigns = %{tabs_style: style}
+
+        ~H"""
+        <.tabs tabs_style={@tabs_style}>
+          <.tab>Tab 1</.tab>
+        </.tabs>
+        """
+        |> parse_component()
+        |> assert_component("div")
+        |> assert_class("tabs tabs-#{style}")
+      end
+    end
+
+    test "tabs position" do
+      for position <- ~w(top bottom) do
+        assigns = %{tabs_position: position}
+
+        ~H"""
+        <.tabs tabs_position={@tabs_position}>
+          <.tab>Tab 1</.tab>
+        </.tabs>
+        """
+        |> parse_component()
+        |> assert_component("div")
+        |> assert_class("tabs tabs-#{position}")
+      end
     end
   end
 
-  test "tabs styles" do
-    for style <- ~w(box border lift) do
-      assigns = %{tabs_style: style}
+  describe "tabs with buttons" do
+    test "basic button tabs with title" do
+      assigns = %{}
 
-      ~H"""
-      <.tabs tabs_style={@tabs_style}>
-        <:tab>Tab 1</:tab>
-      </.tabs>
-      """
-      |> parse_component()
-      |> assert_component("div")
-      |> assert_class("tabs tabs-#{style}")
+      component =
+        ~H"""
+        <.tabs>
+          <.tab type="button" title="Tab 1" />
+          <.tab type="button" title="Tab 2" active />
+          <.tab type="button" title="Tab 3" disabled />
+        </.tabs>
+        """
+
+      expected_component =
+        ~H"""
+        <div role="tablist" class="tabs">
+          <button class="tab">Tab 1</button>
+          <button class="tab tab-active">Tab 2</button>
+          <button class="tab tab-disabled">Tab 3</button>
+        </div>
+        """
+
+      assert_components(component, expected_component)
+    end
+
+    test "basic button tabs with inner block" do
+      assigns = %{}
+
+      component =
+        ~H"""
+        <.tabs>
+          <.tab type="button">
+            Tab 1 <span>Extra content</span>
+          </.tab>
+          <.tab type="button" active>Tab 2</.tab>
+          <.tab type="button" disabled>Tab 3</.tab>
+        </.tabs>
+        """
+
+      expected_component =
+        ~H"""
+        <div role="tablist" class="tabs">
+          <button class="tab">Tab 1 <span>Extra content</span></button>
+          <button class="tab tab-active">Tab 2</button>
+          <button class="tab tab-disabled">Tab 3</button>
+        </div>
+        """
+
+      assert_components(component, expected_component)
     end
   end
 
-  test "tabs position" do
-    for position <- ~w(top bottom) do
-      assigns = %{tabs_position: position}
+  describe "tabs with radio inputs" do
+    test "basic radio tabs with title" do
+      assigns = %{}
 
-      ~H"""
-      <.tabs tabs_position={@tabs_position}>
-        <:tab>Tab 1</:tab>
-      </.tabs>
-      """
-      |> parse_component()
-      |> assert_component("div")
-      |> assert_class("tabs tabs-#{position}")
+      component =
+        ~H"""
+        <.tabs>
+          <.tab type="radio" title="Tab 1" name="my_radio_tabs" />
+          <.tab id="just_testing" type="radio" title="Tab 2" name="my_radio_tabs" active />
+          <.tab type="radio" title="Tab 3" name="my_radio_tabs" disabled />
+        </.tabs>
+        """
+
+      expected_component =
+        ~H"""
+        <div role="tablist" class="tabs">
+          <input type="radio" name="my_radio_tabs" class="tab" aria-label="Tab 1" />
+          <input
+            type="radio"
+            name="my_radio_tabs"
+            class="tab"
+            aria-label="Tab 2"
+            checked="checked"
+            id="just_testing"
+          />
+          <input type="radio" name="my_radio_tabs" class="tab tab-disabled" aria-label="Tab 3" />
+        </div>
+        """
+
+      assert_components(component, expected_component)
+    end
+
+    test "raise error when title is not provided" do
+      assigns = %{}
+
+      assert_raise(
+        ArgumentError,
+        "title attribute is required for radio type tab. Radio type tab cannot have inner block content",
+        fn ->
+          ~H"""
+          <.tabs>
+            <.tab type="radio" />
+          </.tabs>
+          """
+          |> parse_component()
+        end
+      )
+    end
+
+    test "radio tabs with tab content" do
+      assigns = %{}
+
+      component =
+        ~H"""
+        <.tabs tabs_style="lift" tabs_position="bottom">
+          <.tab type="radio" title="Tab 1" name="my_tabs_with_content" />
+          <.tab_content class="bg-base-100 border-base-300 p-6">Tab content 1</.tab_content>
+          <.tab type="radio" title="Tab 2" name="my_tabs_with_content" active />
+          <.tab_content class="bg-base-100 border-base-300 p-6">Tab content 2</.tab_content>
+          <.tab type="radio" title="Tab 3" name="my_tabs_with_content" disabled />
+          <.tab_content class="bg-base-100 border-base-300 p-6">Tab content 3</.tab_content>
+        </.tabs>
+        """
+
+      expected_component =
+        ~H"""
+        <div role="tablist" class="tabs tabs-lift tabs-bottom">
+          <input type="radio" name="my_tabs_with_content" class="tab" aria-label="Tab 1" />
+          <div class="tab-content bg-base-100 border-base-300 p-6">Tab content 1</div>
+
+          <input
+            type="radio"
+            name="my_tabs_with_content"
+            class="tab"
+            aria-label="Tab 2"
+            checked="checked"
+          />
+          <div class="tab-content bg-base-100 border-base-300 p-6">Tab content 2</div>
+
+          <input type="radio" name="my_tabs_with_content" class="tab tab-disabled" aria-label="Tab 3" />
+          <div class="tab-content bg-base-100 border-base-300 p-6">Tab content 3</div>
+        </div>
+        """
+
+      assert_components(component, expected_component)
     end
   end
 
-  test "radio tabs with content" do
-    assigns = %{}
-
-    component =
-      ~H"""
-      <.tabs type="radio" name="my_radio_tabs_1" tabs_style="border">
-        <:tab>Tab 1</:tab>
-        <:tab_content class="border-base-300 bg-base-100 p-10">Tab content 1</:tab_content>
-        <:tab active>Tab 2</:tab>
-        <:tab_content class="border-base-300 bg-base-100 p-10">Tab content 2</:tab_content>
-        <:tab>Tab 3</:tab>
-        <:tab_content class="border-base-300 bg-base-100 p-10">Tab content 3</:tab_content>
-      </.tabs>
-      """
-
-    component_str = rendered_to_string(component) |> String.replace(~r/\s+/, "") |> String.trim()
-
-    expected_component =
-      ~H"""
-      <div class="tabs tabs-border">
-        <input type="radio" name="my_radio_tabs_1" class="tab" aria-label="Tab 1" />
-        <div class="tab-content border-base-300 bg-base-100 p-10">Tab content 1</div>
-        <input type="radio" name="my_radio_tabs_1" class="tab" aria-label="Tab 2" checked="checked" />
-        <div class="tab-content border-base-300 bg-base-100 p-10">Tab content 2</div>
-        <input type="radio" name="my_radio_tabs_1" class="tab" aria-label="Tab 3" />
-        <div class="tab-content border-base-300 bg-base-100 p-10">Tab content 3</div>
-      </div>
-      """
-
-    expected_component_str =
-      rendered_to_string(expected_component) |> String.replace(~r/\s+/, "") |> String.trim()
-
-    assert component_str == expected_component_str
+  defp assert_components(rendered_component, expected_component) do
+    # IO.puts(rendered_to_string(rendered_component))
+    rendered_component_str = rendered_to_string(rendered_component) |> remove_whitespace()
+    expected_component_str = rendered_to_string(expected_component) |> remove_whitespace()
+    assert rendered_component_str == expected_component_str
   end
 
-  test "label tabs with content" do
-    assigns = %{}
-
-    component =
-      ~H"""
-      <.tabs type="label" name="my_label_tabs_1" tabs_style="border">
-        <:tab>Tab 1</:tab>
-        <:tab_content class="border-base-300 bg-base-100 p-10">Tab content 1</:tab_content>
-        <:tab active>Tab 2</:tab>
-        <:tab_content class="border-base-300 bg-base-100 p-10">Tab content 2</:tab_content>
-        <:tab>Tab 3</:tab>
-        <:tab_content class="border-base-300 bg-base-100 p-10">Tab content 3</:tab_content>
-      </.tabs>
-      """
-
-    component_str = rendered_to_string(component) |> String.replace(~r/\s+/, "") |> String.trim()
-
-    expected_component =
-      ~H"""
-      <div class="tabs tabs-border">
-        <label class="tab">
-          <input type="radio" name="my_label_tabs_1" class="tab" /> Tab 1
-        </label>
-        <div class="tab-content border-base-300 bg-base-100 p-10">Tab content 1</div>
-        <label class="tab">
-          <input type="radio" name="my_label_tabs_1" class="tab" checked="checked" /> Tab 2
-        </label>
-        <div class="tab-content border-base-300 bg-base-100 p-10">Tab content 2</div>
-        <label class="tab">
-          <input type="radio" name="my_label_tabs_1" class="tab" /> Tab 3
-        </label>
-        <div class="tab-content border-base-300 bg-base-100 p-10">Tab content 3</div>
-      </div>
-      """
-
-    expected_component_str =
-      rendered_to_string(expected_component) |> String.replace(~r/\s+/, "") |> String.trim()
-
-    assert component_str == expected_component_str
+  defp remove_whitespace(str) do
+    str
+    |> String.replace(~r/\s+/, "")
+    |> String.trim()
   end
 end
