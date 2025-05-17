2d473016f31b8de2cbaba4b64a8917e9f64a3e77
Michael Kompanets <mkompanets@gmail.com>
2025-05-17 16:55:53 -0500

    updating tests to use more robust parsing

--- test/daisy_ui_components/steps_test.exs
+++ test/daisy_ui_components/steps_test.exs
@@ -2,7 +2,6 @@ defmodule DaisyUIComponents.StepsTest do
   use DaisyUIComponents.ComponentCase
 
   import Phoenix.Component
-  import Phoenix.LiveViewTest
   import DaisyUIComponents.Steps
 
   describe "steps component" do
@@ -11,229 +10,117 @@ defmodule DaisyUIComponents.StepsTest do
         assigns = %{color: step_color}
 
         ~H"""
-        <.step color={@color}>My Step</.step>
-        """
-        |> parse_component()
-        |> assert_component("li")
-        |> assert_class("step step-#{step_color}")
-        |> assert_text("My Step")
-      end
-    end
-
-    test "basic steps" do
-      assigns = %{}
-
-      component =
-        ~H"""
         <.steps>
-          <.step color="primary">Register</.step>
-          <.step color="primary">Choose plan</.step>
-          <.step>Purchase</.step>
-          <.step>Receive Product</.step>
+          <.step color={@color}>My Step</.step>
         </.steps>
         """
-
-      expected_component =
-        ~H"""
-        <ul class="steps">
-          <li class="step step-primary">Register</li>
-          <li class="step step-primary">Choose plan</li>
-          <li class="step">Purchase</li>
-          <li class="step">Receive Product</li>
-        </ul>
-        """
-
-      assert_components(component, expected_component)
+        |> parse_component()
+        |> assert_component("ul")
+        |> assert_class("steps")
+        |> assert_children("li", fn step ->
+          step
+          |> assert_class("step step-#{step_color}")
+          |> assert_text("My Step")
+        end)
+      end
     end
 
     test "vertical steps" do
       assigns = %{}
 
-      component =
-        ~H"""
-        <.steps vertical>
-          <.step color="primary">Register</.step>
-          <.step color="primary">Choose plan</.step>
-          <.step>Purchase</.step>
-          <.step>Receive Product</.step>
-        </.steps>
-        """
-
-      expected_component =
-        ~H"""
-        <ul class="steps steps-vertical">
-          <li class="step step-primary">Register</li>
-          <li class="step step-primary">Choose plan</li>
-          <li class="step">Purchase</li>
-          <li class="step">Receive Product</li>
-        </ul>
-        """
-
-      assert_components(component, expected_component)
+      ~H"""
+      <.steps vertical>
+        <.step color="primary">Register</.step>
+      </.steps>
+      """
+      |> parse_component()
+      |> assert_component("ul")
+      |> assert_class("steps steps-vertical")
+      |> assert_children("li", fn step ->
+        step
+        |> assert_class("step step-primary")
+        |> assert_text("Register")
+      end)
     end
 
     test "horizontal steps" do
       assigns = %{}
 
-      component =
-        ~H"""
-        <.steps horizontal>
-          <.step color="primary">Register</.step>
-          <.step color="primary">Choose plan</.step>
-          <.step>Purchase</.step>
-          <.step>Receive Product</.step>
-        </.steps>
-        """
-
-      expected_component =
-        ~H"""
-        <ul class="steps steps-horizontal">
-          <li class="step step-primary">Register</li>
-          <li class="step step-primary">Choose plan</li>
-          <li class="step">Purchase</li>
-          <li class="step">Receive Product</li>
-        </ul>
-        """
-
-      assert_components(component, expected_component)
+      ~H"""
+      <.steps horizontal>
+        <.step color="primary">Register</.step>
+      </.steps>
+      """
+      |> parse_component()
+      |> assert_component("ul")
+      |> assert_class("steps steps-horizontal")
+      |> assert_children("li", fn step ->
+        step
+        |> assert_class("step step-primary")
+        |> assert_text("Register")
+      end)
     end
 
     test "steps with icons" do
       assigns = %{}
 
-      component =
-        ~H"""
-        <.steps>
-          <.step color="neutral" icon="😕">
-            Step 1
-          </.step>
-          <.step color="neutral" icon="😃">
-            Step 2
-          </.step>
-          <.step icon="😍">
-            Step 3
-          </.step>
-        </.steps>
-        """
-
-      expected_component =
-        ~H"""
-        <ul class="steps">
-          <li class="step step-neutral">
-            <span class="step-icon">😕</span>Step 1
-          </li>
-          <li class="step step-neutral">
-            <span class="step-icon">😃</span>Step 2
-          </li>
-          <li class="step">
-            <span class="step-icon">😍</span>Step 3
-          </li>
-        </ul>
-        """
-
-      assert_components(component, expected_component)
+      ~H"""
+      <.steps>
+        <.step icon="😕">
+          Step 1
+        </.step>
+      </.steps>
+      """
+      |> parse_component()
+      |> assert_component("ul")
+      |> assert_class("steps")
+      |> assert_children("li", fn step ->
+        step
+        |> assert_class("step")
+        |> assert_children("span", fn icon ->
+          icon
+          |> assert_class("step-icon")
+          |> assert_text("😕")
+        end)
+      end)
     end
 
     test "steps with content" do
       assigns = %{}
 
-      component =
-        ~H"""
-        <.steps>
-          <.step content="?" color="neutral">Step 1</.step>
-          <.step content="!" color="neutral">Step 2</.step>
-          <.step content="✓" color="neutral">Step 3</.step>
-          <.step content="✕" color="neutral">Step 4</.step>
-          <.step content="★" color="neutral">Step 5</.step>
-          <.step content="" color="neutral">Step 6</.step>
-          <.step content="●" color="neutral">Step 7</.step>
-        </.steps>
-        """
-
-      expected_component =
-        ~H"""
-        <ul class="steps">
-          <li data-content="?" class="step step-neutral">Step 1</li>
-          <li data-content="!" class="step step-neutral">Step 2</li>
-          <li data-content="✓" class="step step-neutral">Step 3</li>
-          <li data-content="✕" class="step step-neutral">Step 4</li>
-          <li data-content="★" class="step step-neutral">Step 5</li>
-          <li data-content="" class="step step-neutral">Step 6</li>
-          <li data-content="●" class="step step-neutral">Step 7</li>
-        </ul>
-        """
-
-      assert_components(component, expected_component)
+      ~H"""
+      <.steps>
+        <.step content="★">Step 1</.step>
+      </.steps>
+      """
+      |> parse_component()
+      |> assert_component("ul")
+      |> assert_class("steps")
+      |> assert_children("li", fn step ->
+        step
+        |> assert_class("step")
+        |> assert_attribute("data-content", "★")
+        |> assert_text("Step 1")
+      end)
     end
 
     test "steps with colors and error state" do
       assigns = %{}
 
-      component =
-        ~H"""
-        <.steps>
-          <.step color="info">Fly to moon</.step>
-          <.step color="info">Shrink the moon</.step>
-          <.step color="info">Grab the moon</.step>
-          <.step color="error" content="?">Sit</.step>
-        </.steps>
-        """
-
-      expected_component =
-        ~H"""
-        <ul class="steps">
-          <li class="step step-info">Fly to moon</li>
-          <li class="step step-info">Shrink the moon</li>
-          <li class="step step-info">Grab the moon</li>
-          <li data-content="?" class="step step-error">Sit</li>
-        </ul>
-        """
-
-      assert_components(component, expected_component)
+      ~H"""
+      <.steps>
+        <.step color="error" content="?">Sit</.step>
+      </.steps>
+      """
+      |> parse_component()
+      |> assert_component("ul")
+      |> assert_class("steps")
+      |> assert_children("li", fn step ->
+        step
+        |> assert_class("step step-error")
+        |> assert_attribute("data-content", "?")
+        |> assert_text("Sit")
+      end)
     end
-
-    test "steps with multiple styles" do
-      assigns = %{}
-
-      component =
-        ~H"""
-        <.steps>
-          <.step color="neutral">Step 1</.step>
-          <.step color="primary">Step 2</.step>
-          <.step color="secondary">Step 3</.step>
-          <.step color="accent">Step 4</.step>
-          <.step color="success">Step 5</.step>
-          <.step color="warning">Step 6</.step>
-          <.step color="error">Step 7</.step>
-        </.steps>
-        """
-
-      expected_component =
-        ~H"""
-        <ul class="steps">
-          <li class="step step-neutral">Step 1</li>
-          <li class="step step-primary">Step 2</li>
-          <li class="step step-secondary">Step 3</li>
-          <li class="step step-accent">Step 4</li>
-          <li class="step step-success">Step 5</li>
-          <li class="step step-warning">Step 6</li>
-          <li class="step step-error">Step 7</li>
-        </ul>
-        """
-
-      assert_components(component, expected_component)
-    end
-  end
-
-  defp assert_components(rendered_component, expected_component) do
-    rendered_component_str = rendered_to_string(rendered_component) |> remove_whitespace()
-    expected_component_str = rendered_to_string(expected_component) |> remove_whitespace()
-    assert rendered_component_str == expected_component_str
-  end
-
-  defp remove_whitespace(str) do
-    str
-    |> String.replace(~r/\s+/, "")
-    |> String.trim()
   end
 end
