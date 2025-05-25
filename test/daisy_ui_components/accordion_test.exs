46be71972de05b877e7b4e8dd1f8dc79ad7ee26e
Dominic Rubas <d@rubas.ch>
2025-05-25 11:48:08 +0200

    Fix Format

--- test/daisy_ui_components/accordion_test.exs
+++ test/daisy_ui_components/accordion_test.exs
@@ -16,11 +16,12 @@ defmodule DaisyUIComponents.AccordionTest do
     |> parse_component()
     |> assert_component("div")
     |> assert_class("collapse")
-    |> assert_children("input[type='radio']", fn child -> assert_attribute(child, "name", "radio_group") end)
+    |> assert_children("input[type='radio']", fn child ->
+      assert_attribute(child, "name", "radio_group")
+    end)
     |> assert_children("div.collapse-title", fn child -> assert_text(child, "title") end)
     |> assert_children("div.collapse-content", fn child -> assert_text(child, "content") end)
   end
-
 
   test "accordion boolean assigns" do
     for boolean_assign <- ~w(arrow plus open close)a do
