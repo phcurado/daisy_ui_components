7497b6b3322c465e7bd7c73e5f51a3bf8650ebde
Dominic Rubas <d@rubas.ch>
2025-05-25 11:13:01 +0200

    Alert Options

--- test/daisy_ui_components/alert_test.exs
+++ test/daisy_ui_components/alert_test.exs
@@ -1,5 +1,6 @@
 defmodule DaisyUIComponents.AlertTest do
-  use ExUnit.Case
+  use DaisyUIComponents.ComponentCase
+
   import Phoenix.Component
   import Phoenix.LiveViewTest
 
@@ -24,6 +25,30 @@ defmodule DaisyUIComponents.AlertTest do
       assert rendered_to_string(~H"""
              <.alert color={@color}>My alert</.alert>
              """) =~ ~s(<div class="alert alert-#{color}">)
+    end
+  end
+
+  test "alert boolean assigns" do
+    for boolean_assign <- ~w(soft dash outline)a do
+      assigns = %{boolean_assign => true}
+
+      ~H"""
+      <.alert {assigns}>My Alert</.alert>
+      """
+      |> parse_component()
+      |> assert_component("div")
+      |> assert_class("alert alert-#{boolean_assign}")
+      |> assert_text("My Alert")
+    end
+  end
+
+  test "alert directions" do
+    for direction <- ~w(vertical horizontal) do
+      assigns = %{direction: direction}
+
+      assert rendered_to_string(~H"""
+             <.alert direction={@direction}>My Alert</.alert>
+             """) =~ ~s(<div class="alert alert-#{direction}">)
     end
   end
 end
