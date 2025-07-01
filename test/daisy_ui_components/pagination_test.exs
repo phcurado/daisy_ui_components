88de2c0eb337fa9b7831cd20fe87197bf1fdcae1
Wígny <wignybora@gmail.com>
2025-07-01 01:24:54 -0400

    Add integration tests

--- test/daisy_ui_components/pagination_test.exs
+++ test/daisy_ui_components/pagination_test.exs
@@ -1,22 +1,50 @@
 defmodule DaisyUIComponents.PaginationTest do
-  use ExUnit.Case
+  use DaisyUIComponents.IntegrationCase
+
   import Phoenix.Component
-  import Phoenix.LiveViewTest
 
   import DaisyUIComponents.Pagination
 
   test "pagination" do
     assigns = %{page: 1, page_size: 10, total_entries: 100}
 
-    pagination =
-      rendered_to_string(~H"""
-      <.pagination />
-      """)
+    template = ~H"""
+    <.pagination />
+    """
+
+    pagination = rendered_to_string(template)
 
     assert pagination =~ ~s(<div class="join">)
     assert pagination =~ ~s(<button class="btn join-item")
     assert pagination =~ ~s(<button class="btn btn-active join-item")
     assert pagination =~ ~s(<button class="btn btn-disabled join-item")
+  end
+
+  test "triggers event on page click", %{conn: conn} do
+    defmodule PageLive do
+      use Phoenix.LiveView
+      import DaisyUIComponents.Pagination
+
+      def render(assigns) do
+        ~H"""
+        <p>Current page: {@page}</p>
+        <.pagination page={@page} page_size={10} total_entries={100} />
+        """
+      end
+
+      def mount(_params, _session, socket) do
+        {:ok, assign(socket, :page, 1)}
+      end
+
+      def handle_event("page_click", %{"page" => page}, socket) do
+        {:noreply, assign(socket, :page, page)}
+      end
+    end
+
+    {:ok, view, html} = live_isolated(conn, PageLive)
+
+    html =~ "Current page: 1"
+    assert render_click(element(view, "button", "2")) =~ "Current page: 2"
   end
 
   test "calculate_display_btn" do
