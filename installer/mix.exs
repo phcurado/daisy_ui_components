2e07e03475aea74bdcb7a03b2dc6af6f3342b926
Paulo Curado <phcurado@gmail.com>
2025-09-30 08:03:01 +0300

    release installer 0.2.2

--- installer/mix.exs
+++ installer/mix.exs
@@ -6,7 +6,7 @@ end
 defmodule Installer.MixProject do
   use Mix.Project
 
-  @version "0.2.1"
+  @version "0.2.2"
   @source_url "https://github.com/phcurado/daisy_ui_components/tree/main/installer"
 
   def project do
