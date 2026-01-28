d050b7af13e7ca133ca445e103ed4d2497c6d47d
Wígny <wignybora@gmail.com>
2026-01-28 14:21:56 -0400

    Use `phx-no-format` to skip formatter
    https://hexdocs.pm/phoenix_live_view/Phoenix.LiveView.HTMLFormatter.html#module-skip-formatting

--- .formatter.exs
+++ .formatter.exs
@@ -1,9 +1,5 @@
 [
   plugins: [Phoenix.LiveView.HTMLFormatter],
   import_deps: [:phoenix],
-  inputs:
-    Enum.flat_map(
-      ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"],
-      &Path.wildcard(&1, match_dot: true)
-    ) -- ["lib/daisy_ui_components/form.ex", "lib/daisy_ui_components/input.ex"]
+  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"]
 ]
--- lib/daisy_ui_components/input.ex
+++ lib/daisy_ui_components/input.ex
@@ -217,7 +217,15 @@ defmodule DaisyUIComponents.Input do
       |> assign_new(:value, fn -> nil end)
 
     ~H"""
-    <.textarea id={@id} name={@name} class={@class} color={@color} ghost={@ghost} {@rest}>{Phoenix.HTML.Form.normalize_value(@type, @value)}</.textarea>
+    <.textarea
+      id={@id}
+      name={@name}
+      class={@class}
+      color={@color}
+      ghost={@ghost}
+      {@rest}
+      phx-no-format
+    >{Phoenix.HTML.Form.normalize_value(@type, @value)}</.textarea>
     """
   end
 
