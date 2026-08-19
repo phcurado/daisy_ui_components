bf2adc0ca740b4985b3c9a63a02b9b049f5f9490
Wígny <wignybora@gmail.com>
2026-08-19 14:47:42 +0100

    Ignore `open` attribute on modal dialog (#169)
    The `open` attribute is toggled client side by `show_modal/2` and
    `hide_modal/2`, so mark it as ignored on mount to stop LiveView from
    patching it back on the next diff.
    
    Replicated to the installer template and covered by tests for the
    closed, `open` and `show` cases.

--- test/daisy_ui_components/modal_test.exs
+++ test/daisy_ui_components/modal_test.exs
@@ -1,9 +1,11 @@
 defmodule DaisyUIComponents.ModalTest do
-  use ExUnit.Case
+  use DaisyUIComponents.ComponentCase
 
   import Phoenix.Component
   import Phoenix.LiveViewTest
   import DaisyUIComponents.Modal
+
+  alias Phoenix.LiveView.JS
 
   describe "modal" do
     test "simple dialog modal" do
@@ -50,6 +52,47 @@ defmodule DaisyUIComponents.ModalTest do
       assert modal =~ ~s(Action from component)
       assert modal =~ ~s(First Action)
       assert modal =~ ~s(Second Action)
+    end
+
+    test "closed modal only ignores the open attribute" do
+      assigns = %{}
+
+      assert ~H"""
+             <.modal id="confirm">
+               my dialog modal
+             </.modal>
+             """
+             |> parse_component()
+             |> assert_component("dialog")
+             |> attribute("phx-mounted") == Jason.encode!(JS.ignore_attributes("open"))
+    end
+
+    test "open modal ignores the open attribute before showing" do
+      assigns = %{}
+
+      assert ~H"""
+             <.modal id="confirm" open>
+               my dialog modal
+             </.modal>
+             """
+             |> parse_component()
+             |> assert_component("dialog")
+             |> attribute("phx-mounted") ==
+               Jason.encode!(show_modal(JS.ignore_attributes("open"), "confirm"))
+    end
+
+    test "shown modal ignores the open attribute before showing" do
+      assigns = %{}
+
+      assert ~H"""
+             <.modal id="confirm" show>
+               my dialog modal
+             </.modal>
+             """
+             |> parse_component()
+             |> assert_component("dialog")
+             |> attribute("phx-mounted") ==
+               Jason.encode!(show_modal(JS.ignore_attributes("open"), "confirm"))
     end
   end
 end
